bcd874aa91987cff6ef69a861f2f82f281769f51
NQT <nguyenquoctuan2995@gmail.com>
2025-05-29 16:53:29 +0700

    fix: update

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ const retrySendMessage = async (channel, message, retries = 3, delay = 5000) => 
 const reminders = [
     { time: '0 59 8 * * 1-5', message: 'Bắt đầu ASAKAI thôi mọi người!' }, // 08:59 thứ 2-6
     { time: '0 0 12 * * 1-5', message: 'Nghỉ trưa thôi mọi người' }, // 12:00 thứ 2-6
-    { time: '0 53 16 * * 1-5', message: 'Nhớ đừng quên daily report nhé: https://work-report.thk-hd-hn.vn/' }, // 16:45 thứ 2-6
+    { time: '0 55 16 * * 1-5', message: 'Nhớ đừng quên daily report nhé: https://work-report.thk-hd-hn.vn/' }, // 16:45 thứ 2-6
 ];
 
 client.once('ready', async () => {
